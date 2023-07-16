87a73e7d123c6fdf0a8413749856b57178ec986f
shradham2612 <mahajanss_1@rknec.edu>
2023-07-16 23:20:17 +0530

    learderboard button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -73,6 +73,7 @@ const Main = () => {
              </button>
             </Link>
             <Link to={"LeaderB"} >
+              
              <button className="bg-white hover:bg-gwhite p-1
              sm:p-4 rounded-full font-medium hover:font-semibold text-xl lg:text-xl text-dblue hover:text-blue-600 mt-10 lg:mt-4">
              LeaderBoard
